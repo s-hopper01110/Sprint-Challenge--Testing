bca591596be663fa144c10e56cc694636cd437c8
Sheila <s.hopper01110@gmail.com>
2019-03-15 13:45:31 -0400

    finished the post tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -12,8 +12,41 @@ describe('server.js', () => {
         .then(res => {
           expect(res.status).toBe(200);
         });
-    });
-
-    
+    })
 })
+
+describe('POST /games', () => {
+  it('should return 422 ', () => {
+    return request(server)
+      .post('/games')
+      .then(res => {
+        expect(res.status).toBe(422);
+      });
+  })
+
+  it('should return 201 created ', () => {
+    const testGames = {id:4, title: 'Sims' , releaseYear: 2000}
+    return request(server)
+      .post('/games')
+      .send(testGames)
+      .then(res => {
+        expect(res.status).toBe(201);
+      });
+  })
+  it('should return JSON', () => {
+    return request(server).post('/games')
+    .then(res => {
+      expect(res.type).toEqual('application/json');
+    });
+    
+  });
+
+})
+
+
+
+
+
+
+
 })
